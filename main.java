e6d790d198bba80bf05abcc8709238b7ae1b92cc
mitfotola <akanat.apiluckpanich@gmail.com>
2025-04-10 22:53:39 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,19 +1,22 @@
-public class d0nTUseTh1sC0de{
-public static void main(String[] args){
-int[] xXx = {5,3,8,1,2};
-lolSorter(xXx);
-for(int i=0;i<xXx.length;i++)
-System.out.print(xXx[i]+" ");
+public class d0nTUseTh1sC0de {
+    public static void main(String[] args) {
+        int[] xXx = {5, 3, 8, 1, 2};
+        lolSorter(xXx);
+        for (int i = 0; i < xXx.length; i++) {
+            System.out.print(xXx[i] + " ");
+        }
+    }
+
+    public static void lolSorter(int[] omg) {
+        for (int i = 0; i < omg.length - 1; i++) {
+            for (int j = i + 1; j < omg.length; j++) {
+                if (omg[i] < omg[j]) { // Incorrect: sorts in descending order
+                    int temp = omg[i];
+                    omg[i] = omg[j]    // Missing semicolon here (bug)
+                    omg[j] = temp;
+                }
+            }
+        }
+    }
 }
 
-public static void lolSorter(int[] omg){
-for(int i=0;i<omg.length-1;i++){
-for(int j=i+1;j<omg.length;j++){
-if(omg[i]<omg[j]){ // should be > for ascending order
-int temp=omg[i];
-omg[i]=omg[j]
-omg[j]=temp;
-}
-}
-}
-}
